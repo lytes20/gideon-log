92dd209fc3b4d8c3a6725ef713f8d8f899ac18d0
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-11-21 00:52:31 -0600

    Delete commented out code

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import "./styles/Home.css";
 import Typewriter from "./components/Typewriter";
 
 const myAdjectives = ["software engineer", "builder", "problem solver"];
 function Home() {
-  // const [typedText, setTypedText] = useState("");
-  // useEffect(() => {
-  //   const text = "software engineer";
-  //   let i = 0;
-  //   const interval = setInterval(() => {
-  //     const nextChar = text.charAt(i);
-  //     setTypedText((prev) => prev + nextChar);
-  //     i++;
-  //     if (i >= text.length) {
-  //       clearInterval(interval);
-  //     }
-  //   }, 50);
-  //   return () => clearInterval(interval);
-  // }, []);
-
   return (
     <div>
       <Header parent="home" />
@@ -28,9 +12,7 @@ function Home() {
           <h1>Hi, my name is Gideon Bamuleseyo</h1>
           <div className="typewriter-container">
             <h2>I am a</h2>
-            {/* <h2 id="typewriter" className="">
-              {typedText}
-            </h2> */}
+
             <Typewriter words={myAdjectives} />
             <h2 className="caret">_</h2>
           </div>
