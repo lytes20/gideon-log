5be07c3dce4de0b8fef979339d6c102133f8453c
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-11-24 15:29:59 -0600

    Lessons from anime

--- src/blog/BlogData.ts
+++ src/blog/BlogData.ts
@@ -1,6 +1,7 @@
 import Cache from "./blogs/cache.mdx";
 import Iteration from "./blogs/iteration.mdx";
 import RemoteWork from "./blogs/remote-work.mdx";
+import Training from "./blogs/training.mdx";
 
 interface blogProps {
   title: string;
@@ -11,4 +12,9 @@ export const blogs: blogProps[] = [
   { title: "Caching", path: "/caching", component: Cache },
   { title: "Iteration", path: "/iteration", component: Iteration },
   { title: "Remote Work", path: "/remote-work", component: RemoteWork },
+  {
+    title: "Lessons From Anime",
+    path: "/lessons-from-anime",
+    component: RemoteWork,
+  },
 ];
