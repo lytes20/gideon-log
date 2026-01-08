f3dad34ad8dfdd87ed2c6e4d10d6d729f938ac57
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-01-07 21:14:00 -0600

    Add container styles

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -2,7 +2,7 @@ import Timeline from "../components/Timeline";
 
 function Experience() {
   return (
-    <div>
+    <div className="container">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Experience
