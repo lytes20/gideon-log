90c6499645b593c199ddb40538b0c6b1f019c8a6
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-02-09 22:40:25 -0600

    Archive blog data

--- src/blog/BlogData.ts
+++ src/blog/BlogData.ts
@@ -1,5 +1,4 @@
 import Cache from "./blogs/cache.mdx";
-import RemoteWork from "./blogs/remote-work.mdx";
 import Lessons from "./blogs/lessons.mdx";
 import CreatingReactApp from "./blogs/creating-a-react-app.mdx";
 import FetchingData from "./blogs/fetch.mdx";
@@ -27,7 +26,6 @@ export const blogs: blogProps[] = [
     component: CreatingReactApp,
   },
   { title: "Caching", path: "/caching", component: Cache },
-  { title: "Remote Work", path: "/remote-work", component: RemoteWork },
   {
     title: "Some Lessons (With some anime inspiration)",
     path: "/lessons",
