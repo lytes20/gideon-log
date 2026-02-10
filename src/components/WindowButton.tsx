8cfb5c7f69cdf3ca0f18c0cbaa4dac8a1798608c
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-02-09 23:27:51 -0600

    Add window

---
+++ src/components/WindowButton.tsx
@@ -0,0 +1,5 @@
+function WindowButton() {
+  return <div className="w-[10px] h-[10px] bg-gray-500 rounded-lg"></div>;
+}
+
+export default WindowButton;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,34 @@
 import Header from "../components/Header";
 import "../styles/home.css";
 import Typewriter from "../components/Typewriter";
+import WindowButton from "../components/WindowButton";
 
-const myAdjectives = ["software engineer", "builder", "problem solver"];
+const MY_ADJECTIVES = ["software engineer", "builder", "problem solver"];
+
 function Home() {
   return (
     <div>
       <Header parent="home" />
-      <div className="main">
-        <div>
-          <h1>Hi, my name is Gideon Bamuleseyo</h1>
-          <div className="typewriter-container">
-            <h2>I am a</h2>
-
-            <Typewriter words={myAdjectives} />
-            <h2 className="caret">_</h2>
+      <div className="main border border-gray-500 rounded-md">
+        <div className="border-b border-gray-500">
+          <div className="flex gap-1 p-1">
+            {["", "", ""].map((btn, index) => (
+              <WindowButton key={index} />
+            ))}
           </div>
         </div>
-        <div>Javascript | Typescript | Java | Python</div>
+        <div className="p-2">
+          <div>
+            <h1>Hi, my name is Gideon Bamuleseyo</h1>
+            <div className="typewriter-container">
+              <h2>I am a</h2>
+
+              <Typewriter words={MY_ADJECTIVES} />
+              <h2 className="caret">_</h2>
+            </div>
+          </div>
+          <div>Javascript | Typescript | Java | Python</div>
+        </div>
       </div>
       <div className="mx-auto w-[80%] text-center mt-16">
         <h1 className="text-lg">
